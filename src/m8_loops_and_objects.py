14b61e66889148081a597eadcc7e7157de74b832
kaysm <kaysm@rose-hulman.edu>
2019-02-01 15:51:10 -0500

    l

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -18,11 +18,11 @@ def main():
     #print_sequence1()
     #print_sequence2()
     #print_sequence3()
-    draw_circles1()
+    #draw_circles1()
     draw_circles2()
-    draw_circles3()
+    #draw_circles3()
     #print_cosines()
-    draw_cosines_and_sines()
+    #draw_cosines_and_sines()
 
 
 
